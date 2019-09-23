3e96b2868e337461d21ba9d80b921ea8c9e46926
Amritpal Maan <sonny_maan@yahoo.com>
2019-09-23 14:22:24 +0100

    seperated the number into individual digits with .digits

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,9 +1,9 @@
 require_relative 'spec_helper'
 require_relative '../lib/app.rb'
 
-describe 'decending_order' do
-  it "takes any integer and returns it in decending order" do
-    ordered_1 = decending_order(15872)
+describe 'descending_order' do
+  it "takes any integer and returns it in descending order" do
+    ordered_1 = descending_order(15872)
     expect(ordered_1).to eq (87521)
 end
 end
