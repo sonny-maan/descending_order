e990aef719ba52ac964d412466a2ec0e09806c79
Amritpal Maan <sonny_maan@yahoo.com>
2019-09-23 14:40:46 +0100

    ran rubocop linter

--- lib/app.rb
+++ lib/app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def descending_order(num)
   num.digits.sort.reverse.join.to_i
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 require_relative 'spec_helper'
 require_relative '../lib/app.rb'
 
 describe 'descending_order' do
-
-  it "takes any integer and returns it in descending order" do
-    ordered_1 = descending_order(15872)
-    expect(ordered_1).to eq (87521)
+  it 'takes any integer and returns it in descending order' do
+    ordered_1 = descending_order(15_872)
+    expect(ordered_1).to eq 87_521
   end
 
-  it "returns the original number if only one number is entered" do
+  it 'returns the original number if only one number is entered' do
     edge_case1 = descending_order(1)
     edge_case2 = descending_order(0)
-    expect(edge_case1).to eq (1)
-    expect(edge_case2).to eq (0)
+    expect(edge_case1).to eq 1
+    expect(edge_case2).to eq 0
   end
 end
