1ae223fd6f4e9f50e84b0eb73c088e80fe7da59c
Amritpal Maan <sonny_maan@yahoo.com>
2019-09-23 14:29:53 +0100

    added test for edge case (only one number entered)

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,8 +2,14 @@ require_relative 'spec_helper'
 require_relative '../lib/app.rb'
 
 describe 'descending_order' do
+
   it "takes any integer and returns it in descending order" do
     ordered_1 = descending_order(15872)
     expect(ordered_1).to eq (87521)
-end
+  end
+
+  it "returns the origional number if only one number is entered" do
+    edge_case1 = descending_order(1)
+    expect(edge_case1).to eq (1)
+  end
 end
